d67282603562de403372f4eabe706f934b514cd0
cedrusx <shixuesong@gmail.com>
2016-05-19 16:05:15 +0800

    le-6-zigzag-conversion Using pointer instead of iterator

--- le-6-zigzag-conversion.cpp
+++ le-6-zigzag-conversion.cpp
@@ -1,3 +1,4 @@
+// 20ms
 class Solution {
 public:
     string convert(string s, int numRows) {
@@ -9,6 +10,26 @@ public:
                 res.push_back(*c);
                 if (row != 0 && row != numRows - 1 && c + 2 * (numRows - row - 1) < s.end())
                     res.push_back(*(c + 2 * (numRows - row - 1)));
+            }
+        }
+        return res;
+    }
+};
+
+// 16ms
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows <= 1)
+            return s;
+        auto size = s.size();
+        string res(size, 0);
+        string::size_type rid = 0;
+        for (int row = 0; row < numRows; row++) {
+            for (string::size_type sid = row; sid < size; sid += (2 * numRows - 2)) {
+                res[rid++] = s[sid];
+                if (row != 0 && row != numRows - 1 && sid + 2 * (numRows - row - 1) < size)
+                    res[rid++] = s[sid + 2 * (numRows - row - 1)];
             }
         }
         return res;
